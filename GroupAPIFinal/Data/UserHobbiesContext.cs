a56028394a5ac61a8b0d037a7d2dcaa9c2ba160c
rawlinsdrew3 <rawlinaj@mail.uc.edu>
2023-04-25 19:52:54 -0400

    Contexts

---
+++ GroupAPIFinal/Data/UserHobbiesContext.cs
@@ -0,0 +1,22 @@
+﻿using FinalProjectNetAPI.Data;
+using GroupAPIFinal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroupAPIFinal.Data
+{
+    public class UserHobbiesContext
+    {
+        public UserHobbiesContext(DbContextOptions<UserHobbiesContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<UserHobbies>().HasData(
+                new UserHobbies { Id = 1,Hobby1 = "Painting", Hobby2 = "Drawing", Hobby3 = "Photography", Hobby4 = "Knitting" },
+                new UserHobbies { Id = 2, Hobby1 = "Ohio", Hobby2 = "Milford", Hobby3 = 513, Hobby4 = 45150 },
+                new UserHobbies { Id = 3, Hobby1 = "Ohio", Hobby2 = "Columbus", Hobby3 = 614, Hobby4 = 43004 }//add new data here, copy line above and change fields
+                );
+        }
+
+        public DbSet<UserHobbies> Hobbies { get; set; }
+    }
+}
---
+++ GroupAPIFinal/Data/UserMusicContext.cs
@@ -0,0 +1,22 @@
+﻿using FinalProjectNetAPI.Data;
+using GroupAPIFinal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroupAPIFinal.Data
+{
+    public class UserMusicContext
+    {
+        public UserMusicContext(DbContextOptions<UserMusicContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<UserMusic>().HasData(
+                new UserMusic { Id = 1, FavBand = "The Beatles", FavArtist = "Elvis Pressley", FavSong = "Here Comes The Sun", FavGenre = "Classic" },
+                new UserMusic { Id = 1, FavBand = "Dave Matthews Band", FavArtist = "Micheal Jackson", FavSong = "Billie Jean", FavGenre = "Pop" },
+                new UserMusic { Id = 1, FavBand = "AC/DC", FavArtist = "George Strait", FavSong = "Troubadour", FavGenre = "Country" }//add new data here, copy line above and change fields
+                );
+        }
+
+        public DbSet<UserMusic> Music { get; set; }
+    }
+}
