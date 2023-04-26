f2344bc35a022f0e48722c1e3c1a4fe569f544f2
JDC11202 <j.collier11202@gmail.com>
2023-04-25 22:26:45 -0400

    Fixed UserLocationInfo

--- GroupAPIFinal/Controllers/HobbiesController.cs
+++ GroupAPIFinal/Controllers/HobbiesController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace GroupAPIFinal.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class HobbiesController : ControllerBase
     {
 
